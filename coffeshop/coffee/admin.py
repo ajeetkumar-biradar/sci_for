4c37006d8af6d468028a928d1d5ba9dcd37c318b
Ajeetkumar Biradar <136881345+ajeetkumar-biradar@users.noreply.github.com>
2024-07-02 15:59:28 +0530

    Delete coffeshop directory

--- coffeshop/coffee/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Coffe
-
-
-class CoffeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity')
-
-
-admin.site.register(Coffe, CoffeeAdmin)
--- coffeshop/coffee/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Coffe
-
-
-def home(request):
-    coffee = Coffe.objects.all()
-    return render(request, 'home.html', {'coffee': coffee})
