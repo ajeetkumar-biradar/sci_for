cc954dc959c6965644e55e907f78f8e096664713
Ajeetkumar Biradar <136881345+ajeetkumar-biradar@users.noreply.github.com>
2024-07-02 15:59:42 +0530

    Delete django_project directory

--- django_project/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.task_list, name='task_list'),
-    path('tasks/<int:pk>/', views.task_detail, name='task_detail'),
-    path('tasks/new/', views.task_new, name='task_new'),
-    path('tasks/<int:pk>/edit/', views.task_edit, name='task_edit'),
-]
